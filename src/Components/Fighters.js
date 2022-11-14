71b791461074711317122e30b1c6c5e88ae7b3fc
Hamzaali7 <Hamzaali7@yahoo.com>
2022-11-14 13:55:25 -0500

    player components added

--- src/Components/Fighters.js
+++ src/Components/Fighters.js
@@ -3,7 +3,12 @@ import React from 'react';
 function Fighters(){
     return(
         <div>
-            
+            <form className="player-one">
+
+            </form>
+            <form className="player-two">
+                
+            </form>
         </div>
     )
 }
